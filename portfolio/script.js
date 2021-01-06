99099a00cd77d6efdbfcff008cbe3f9564047024
Jayson1307 <2125083@talnet.nl>
2021-01-06 14:26:54 +0100

    klaar

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,22 +1,14 @@
-var naam = prompt("vul je naam hier in");
+console.log("test")
+var naam = prompt("vul je naam hier in")
     if (naam != null) {
       document.getElementById("demo").innerHTML =
       "Hallo " + naam +". En welkom bij mijn portofolio." ;
     }
-
-
-
-var leeftijd = prompt("hoe oud ben je");
-if(leeftijd >= 18){
-    document.body.style.backgroundColor = "green";
-}
-if(leeftijd <= 17){
-    document.body.style.backgroundColor = "red";
-}
-
-//console
-let bezoeker ={
-    naam:naam,
-    leeftijd:leeftijd
-}
-console.log(bezoeker);
+    -leeftijd
+    var leeftijd = prompt("wat is je leeftijd?");
+    if (leeftijd >= 18){
+        document.body.style.backgroundColor = "lightgreen";
+    }
+    if (leeftijd < 18){
+        document.body.style.backgroundColor = "rgb(238, 106, 106)";
+    }
